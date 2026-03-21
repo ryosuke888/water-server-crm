a1c569e1e3d6c2e0582fdbf38433f26048582f25
ryosuke888 <ryosuke8sr@gmail.com>
2026-03-21 17:24:46 +0900

    顧客一覧ページ作成

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -3,8 +3,75 @@
 
     </x-slot>
 
-    <x-slot>
-        <div>
+    <div class="py-8">
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div class="overflow-hidden rounded-2xl bg-white shadow-sm ring-1 ring-gray-200">
+                <table class="w-full table-fixed divide-y divide-gray-200 text-sm">
+                    <thead class="bg-gray-50">
+                        <tr>
+                            <th class="w-[20%] px-4 py-4 text-left font-semibold text-gray-600">顧客名</th>
+                            <th class="w-[20%] px-4 py-4 text-left font-semibold text-gray-600">電話番号</th>
+                            <th class="w-[20%] px-4 py-4 text-left font-semibold text-gray-600">メールアドレス</th>
+                            <th class="w-[20%] px-4 py-4 text-left font-semibold text-gray-600">契約状況</th>
+                            <th class="w-[20%] px-4 py-4 text-left font-semibold text-gray-600">登録日</th>
+                        </tr>
+                    </thead>
+                    <tbody class="divide-y divide-gray-100 bg-white">
+                        <tr class="hover:bg-gray-50">
+                            <td class="px-4 py-4 text-gray-700 text-left">
+                                <div class="truncate">テストテスト</div>
+                            </td>
+                            <td class="px-4 py-4 text-gray-700 text-left">
+                                <div class="truncate">テストテストテストテストテストテストテスト</div>
+                            </td>
+                            <td class="px-4 py-4 text-gray-700 text-left">
+                                <div class="truncate">テストテストテストテストテストテストテスト</div>
+                            </td>
+                            <td class="px-4 py-4 text-gray-700 text-left">
+                                <div class="truncate">テストテストテストテストテストテストテスト</div>
+                            </td>
+                            <td class="px-4 py-4 text-gray-700 text-left">
+                                <div class="truncate">2026/03/21</div>
+                            </td>
+                            <tr class="hover:bg-gray-50">
+                            <td class="px-4 py-4 text-gray-700 text-left">
+                                <div class="truncate">テストテスト</div>
+                            </td>
+                            <td class="px-4 py-4 text-gray-700 text-left">
+                                <div class="truncate">テストテストテストテストテストテストテスト</div>
+                            </td>
+                            <td class="px-4 py-4 text-gray-700 text-left">
+                                <div class="truncate">テストテストテストテストテストテストテスト</div>
+                            </td>
+                            <td class="px-4 py-4 text-gray-700 text-left">
+                                <div class="truncate">テストテストテストテストテストテストテスト</div>
+                            </td>
+                            <td class="px-4 py-4 text-gray-700 text-left">
+                                <div class="truncate">2026/03/21</div>
+                            </td>
+                        </tr>
+                        <tr class="hover:bg-gray-50">
+                            <td class="px-4 py-4 text-gray-700 text-left">
+                                <div class="truncate">テストテスト</div>
+                            </td>
+                            <td class="px-4 py-4 text-gray-700 text-left">
+                                <div class="truncate">テストテストテストテストテストテストテスト</div>
+                            </td>
+                            <td class="px-4 py-4 text-gray-700 text-left">
+                                <div class="truncate">テストテストテストテストテストテストテスト</div>
+                            </td>
+                            <td class="px-4 py-4 text-gray-700 text-left">
+                                <div class="truncate">テストテストテストテストテストテストテスト</div>
+                            </td>
+                            <td class="px-4 py-4 text-gray-700 text-left">
+                                <div class="truncate">2026/03/21</div>
+                            </td>
+                        </tr>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </x-slot>
+    </div>
+
 </x-app-layout>
