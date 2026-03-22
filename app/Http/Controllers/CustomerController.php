3a6ad98656622b021044be4e05974fc3d8cf9f5d
ryosuke888 <ryosuke8sr@gmail.com>
2026-03-23 01:51:40 +0900

    顧客登録にtry-catch文追加

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -6,7 +6,11 @@ use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Models\Customer;
 use App\Services\CustomerService;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+
 
 
 class CustomerController extends Controller
@@ -37,8 +41,15 @@ class CustomerController extends Controller
     }
 
     public function store(StoreCustomerRequest $request, CustomerService $customerService) {
-        $validated = $request->validated();
-        $customer = $customerService->store($validated);
-        return redirect()->route('customers.show', $customer);
+        try {
+            $validated = $request->validated();
+            $customer = $customerService->store($validated);
+            return redirect()->route('customers.show', $customer)->with('success', '顧客登録に成功しました。');
+        } catch(Exception $e) {
+            Log::error("顧客登録失敗", [
+                'error' => $e->getMessage(),
+            ]);
+            return back()->withInput()->with('error', '顧客登録に失敗しました。');
+        }
     }
 }
